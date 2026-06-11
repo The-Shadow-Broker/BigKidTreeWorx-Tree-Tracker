22a4d9352c1e5f3caa5cefc9b05bf221e8c26d42
The-Shadow-Broker <shadowbroker4@protonmail.com>
2026-06-11 01:06:43 -0400

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bigkidtreeworx-tree-tracker/"
+  base: "/BigKidTreeWorx-Tree-Tracker/"
 });
